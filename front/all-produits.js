fc8dcdba5757636355a60b6fafe60500520f9e90
Lefebvre Anthony <anthony.lfb@hotmail.fr>
2021-02-09 09:47:02 +0100

    files order and all produit JS clean

--- front/all-produits.js
+++ front/all-produits.js
@@ -1,79 +1,71 @@
 
-
-
 const buttonRedirect = document.getElementById('i');
 const cardProduit = document.getElementById('produit-card');
 const nameProduit = document.getElementById('nom-produit');
 const imgProduit = document.getElementById('img-produit');
 const mainProduit = document.getElementById('main-produit');
 const buttonAddPanier = document.querySelector('.add-panier');
-const url = "http://localhost:3000/api/teddies";
-
-function allTeddies (){
-    fetch(url)
+const allTeddies = function () {
+    fetch("http://localhost:3000/api/teddies")
         .then(response => {
-
             if (response.ok) {
                 return response.json().then(data => {
+                    try {
+
+                        // Boucle pour chaque objet dans l'api, créer un clone avec les informations de l'objet.
+                        for (i = 0; i < data.length; i++) {
+                            let dataId = data[i]._id;
+                            let dataPrice = data[i].price;
+                            let dataName = data[i].name;
+                            let dataImg = data[i].imageUrl;
+
+                            nameProduit.innerText = dataName;
+                            imgProduit.src = dataImg;
+                            buttonRedirect.id = dataId;
+                            buttonAddPanier.dataset.id = dataId;
+
+                            const addCard = cardProduit.cloneNode(true);
+                            mainProduit.appendChild(addCard);
 
 
-                    //Boucle pour chaque objet dans l'api, créer un clone avec les informations de l'objet.
-                    for (i = 0; i < data.length; i++) {
-                        //console.log(cardProduit)
-                        let dataId = data[i]._id;
-                        let dataPrice = data[i].price;
-                        let dataName = data[i].name;
-                        let dataImg = data[i].imageUrl;
+                            // Afficher la page detail produit avec le bon produit
+                            addCard.querySelector('.detail-button').addEventListener('click', event => {
+                                window.location.href = `./pages/produit.html?id=${event.target.id}`;
+                            });
 
-                        nameProduit.innerText = dataName;
-                        imgProduit.src = dataImg;
-                        buttonRedirect.id = dataId;
-                        buttonAddPanier.dataset.id = dataId;
-
-                        const addCard = cardProduit.cloneNode(true);
-                        mainProduit.appendChild(addCard);
-
-
-                        //Afficher la page detail produit avec le bon produit
-                        const button = addCard.querySelector('.detail-button');
-                        button.addEventListener('click', event => {
-                            window.location.href = `./pages/produit.html?id=${event.target.id}`;
-                        });
-
-
-                        const button2 = addCard.querySelector('.add-panier');
-                        button2.addEventListener('click', event => {
-                            
-                            let cart = localStorage.getItem('cart');
-                            
-                            if(cart == null){
-                                cart = [
-                                    {
-                                        'name': dataName,
-                                        'id': dataId,
-                                        'quantity': 1,
-                                        'price': dataPrice,
-                                        'img': dataImg,
+                            // Ajouter item dans LocalStorage
+                            addCard.querySelector('.add-panier').addEventListener('click', event => {
+                                let cart = localStorage.getItem('cart');
+                                if (cart == null) {
+                                    cart = [
+                                        {
+                                            'name': dataName,
+                                            'id': dataId,
+                                            'quantity': 1,
+                                            'price': dataPrice,
+                                            'img': dataImg,
+                                        }
+                                    ]
+                                } else {
+                                    cart = JSON.parse(cart)
+                                    let finded = 0;
+                                    for (i = 0; i < cart.length; i++) {
+                                        if (dataName === cart[i].name) {
+                                            cart[i].quantity += 1;
+                                            finded = 1;
+                                            break
+                                        }
                                     }
-                                ]
-                            } else{
-                                cart = JSON.parse(cart)
-                                let finded = 0;
-                                for(i = 0; i < cart.length; i++){
-                                    if(dataName === cart[i].name){
-                                        cart[i].quantity += 1;
-                                        finded = 1;
-                                        break
+                                    if (!finded) {
+                                        cart.push({ 'name': dataName, 'id': dataId, 'quantity': 1, 'price': dataPrice, 'img': dataImg, })
                                     }
                                 }
-                                if(!finded){
-                                    cart.push({'name': dataName, 'id': dataId, 'quantity': 1, 'price': dataPrice, 'img': dataImg,})
-                                }
-                            }
-                            localStorage.setItem('cart', JSON.stringify(cart));
-
-                            //window.location.href = './pages/panier.html';
-                        });
+                                localStorage.setItem('cart', JSON.stringify(cart));
+                            });
+                        }
+                    }
+                    catch (e) {
+                        console.log('Erreur ' + e);
                     }
                 });
 
--- front/order.js
+++ front/order.js
@@ -1,14 +1,11 @@
 
 const buttonAllProducts = document.getElementById('all-products');
+const congrat = document.getElementById("congrat");
+let orderId = localStorage.getItem('orderId');
 
+//button Nos produits
 buttonAllProducts.addEventListener('click', function(){
     window.location.href = '../index.html';
 });
-
-let orderId = localStorage.getItem('orderId');
-console.log(orderId)
-
-const congrat = document.getElementById("congrat");
-
 
 congrat.innerHTML = `Félicitation votre commande ${orderId} a bien été passée`;
