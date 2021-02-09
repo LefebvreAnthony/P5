0bc5e7360c55fe4e05cefbce6c38778f368006f1
Lefebvre Anthony <anthony.lfb@hotmail.fr>
2021-02-09 09:30:19 +0100

    clear file panier.html

--- front/panier.js
+++ front/panier.js
@@ -1,26 +1,13 @@
 
-
-const url = "http://localhost:3000/api/teddies";
-
-
-let objCart = localStorage.getItem('cart');
-let objCartParse = JSON.parse(objCart);
+let objCartParse = JSON.parse(localStorage.getItem('cart'));
 const produitCart = document.getElementById('produit-cart');
 const mainCart = document.getElementById('main-cart');
 const buttonClear = document.getElementById('clear-cart');
-const totalPriceCart = document.getElementById('total-price');
-
-
-
-const imgProduit = document.getElementById('img-produit');
-const nameProduit = document.getElementById('name-produit');
-const priceCart = document.getElementById('price-cart');
-const buttonAllProducts = document.getElementById('all-products');
-
-buttonAllProducts.addEventListener('click', function () {
-    window.location.href = '../index.html';
-});
-
+const inputLastName = document.getElementById('inputLastName');
+const inputFirstName = document.getElementById('inputFirstName');
+const inputCity = document.getElementById('inputCity');
+const inputAdress = document.getElementById('inputAdress');
+const inputMail = document.getElementById('inputMail');
 let postForm = {
     contact: {
         firstName: '',
@@ -28,252 +15,149 @@ let postForm = {
         address: '',
         city: '',
         email: '',
-
     },
     products: [],
-
-}
+};
 
 //création visuel des articles que l'on à ajouté au panier
 function viewCart() {
     try {
         if (localStorage.length >= 1) {
-
-            const cartLenght = objCartParse.length;
             let totalPrice = 0;
-            for (let i = 0; i < cartLenght; i++) {
-                let cartName = objCartParse[i].name;
-                nameProduit.innerHTML = cartName;
+            for (let i = 0; i < objCartParse.length; i++) {
+                document.getElementById('name-produit').innerHTML = objCartParse[i].name;
+                document.getElementById('img-produit').src = objCartParse[i].img;
+                document.getElementById('cart-quantity').innerHTML = `Quantité d'article : ${objCartParse[i].quantity.toString()}`;
 
-                let imgCart = objCartParse[i].img;
-                imgProduit.src = imgCart;
-
-                let cartQuantity = document.getElementById('cart-quantity');
-                cartQuantity.innerHTML = `Quantité d'article : ${objCartParse[i].quantity.toString()}`;
                 //Calcul prix des articles
                 let cartPrice = objCartParse[i].price * objCartParse[i].quantity;
-                priceCart.innerHTML = cartPrice + " €";
+                document.getElementById('price-cart').innerHTML = cartPrice + " €";
                 totalPrice += cartPrice;
-                //Calcul total prix du panier
 
+                //Calcul total prix du panier
                 const addCard = produitCart.cloneNode(true);
                 mainCart.appendChild(addCard);
 
-
                 //Créer les valeurs pour le post order de chaque id article
                 for (let j = 0; j < objCartParse[i].quantity; j++) {
-
                     postForm.products.push(objCartParse[i].id);
                 }
             }
-            totalPriceCart.innerHTML = `Prix total  : ${totalPrice} €`;
+            document.getElementById('total-price').innerHTML = `Prix total  : ${totalPrice} €`;
         }
         else {
-            const mainProduit = document.getElementById('main-produit');
-            mainProduit.removeChild(mainCart)
+            document.getElementById('main-produit').removeChild(mainCart);
             //suprimer bouton vider panier
             document.getElementById('parent-button').removeChild(buttonClear);
         }
     } catch (e) {
-        console.log('Erreur : ' + e)
+        console.log('Erreur : ' + e);
     }
 }
 
+//Function Reccup value formulaire 
+const reccupValueForm = function(){
 
-viewCart();
-mainCart.removeChild(produitCart);
-produitCart.removeAttribute('id');
+    document.getElementById('inputValidation').addEventListener('click', function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        postForm.contact = {
+            firstName: inputFirstName.value,
+            lastName: inputLastName.value,
+            address: inputAdress.value,
+            city: inputCity.value,
+            email: inputMail.value,
+        }
+        fetch('http://localhost:3000/api/teddies/order', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postForm),
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json().then(data => {
+                        localStorage.clear();
+                        localStorage.setItem("orderId", data.orderId)
+                        location.href = 'order.html'
+    
+                    })
+                };
+            })
+    });
+};
 
+const eventClick = function(){
+    //Nos produit button
+    document.getElementById('all-products').addEventListener('click', function () {
+        window.location.href = '../index.html';
+    });
 
-//Fonction de vider le panier
-
-function clearCart() {
-
+    //Fonction de vider le panier
     buttonClear.addEventListener('click', event => {
         window.localStorage.clear();
         document.location.replace('panier.html');
         window.alert('Votre panier est supprimé');
-
-    })
-}
-clearCart();
-
-
-
-const inputLastName = document.getElementById('inputLastName');
-const inputFirstName = document.getElementById('inputFirstName');
-const inputCity = document.getElementById('inputCity');
-const inputAdress = document.getElementById('inputAdress');
-const inputMail = document.getElementById('inputMail');
-const inputValidation = document.getElementById('inputValidation');
-
-
-//Function Reccup value formulaire 
-inputValidation.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    postForm.contact = {
-        firstName: inputFirstName.value,
-        lastName: inputLastName.value,
-        address: inputAdress.value,
-        city: inputCity.value,
-        email: inputMail.value,
-
-    }
-    fetch('http://localhost:3000/api/teddies/order', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(postForm),
-    })
-        .then(response => {
-            if (response.ok) {
-                return response.json().then(data => {
-                    localStorage.clear();
-                    localStorage.setItem("orderId", data.orderId)
-                    location.href = 'order.html'
-
-                })
-            };
-        })
-});
-
-console.log(postForm)
-
-
-
+    });
+};
 // ************************************************************************ SERIE DE VALIDATION FORM ************************************************************************
+const validateInuput = function (input, inputName, regex) {
+    const regExp = new RegExp(regex, 'g');
+    let small = input.nextElementSibling;
 
-// Validation caractère Email
-const validateEmail = function (mail) {
-
-    const emailRegExp = new RegExp(
-
-        '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$',
-        'g'
-    );
-    let testEmail = emailRegExp.test(mail.value);
-    let small = inputMail.nextElementSibling;
-    if (testEmail) {
-        small.innerHTML = 'Adresse email valide';
+    if (regExp.test(input.value)) {
+        small.innerHTML = inputName + ' valide';
         small.classList.remove('text-danger');
         small.classList.add('text-success');
+        input.classList.remove('border-danger');
+        input.classList.add('border-success');
 
     } else {
-        small.innerHTML = "Adresse email non valide ! "
+        small.innerHTML = inputName + " invalide ! ";
         small.classList.remove('text-success');
         small.classList.add('text-danger');
+        input.classList.remove('border-success');
+        input.classList.add('border-danger');
+
     }
 };
 
-inputMail.addEventListener('change', function () {
+const validateForm = function(){
 
-    validateEmail(this);
-});
-
-
-
-
-//Validation caratère Nom de famille !!
-const validateLastName = function (lastName) {
-
-    const lastNameRegExp = new RegExp(
-        '^[A-Za-z]{1,20}$',
-        'g'
-    );
-    let testLastName = lastNameRegExp.test(lastName.value)
-    let small = inputLastName.nextElementSibling;
-    if (testLastName) {
-        small.innerHTML = 'Nom valide';
-        small.classList.remove('text-danger');
-        small.classList.add('text-success');
-
-    } else {
-        small.innerHTML = "Nom invalide ! "
-        small.classList.remove('text-success');
-        small.classList.add('text-danger');
-    }
+    // Validation caractère Email
+    inputMail.addEventListener('change', function () {
+        validateInuput(this, 'Email', '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+    });
+    
+    //Validation caratère Nom de famille !!
+    inputLastName.addEventListener('change', function () {
+        validateInuput(this, 'Nom', '^[A-Za-z]{1,20}$');
+    });
+    
+    //Validation caratère Prénom !!
+    inputFirstName.addEventListener('change', function () {
+        validateInuput(this, 'Prénom', '^[A-Za-z]{1,20}$');
+    });
+    
+    //Validation caratère Ville !!
+    inputCity.addEventListener('change', function () {
+        validateInuput(this, 'Ville', '^[a-zA-Z-\\s]{3,50}$');
+    });
+    
+    //Validation caratère Adresse !!
+    inputAdress.addEventListener('change', function () {
+        validateInuput(this, 'Adresse', '^[a-zA-Z0-9-_\\s]{3,50}$');
+    });
 };
-
-
-inputLastName.addEventListener('change', function () {
-    validateLastName(this);
-});
-//Validation caratère Prénom !!
-const validateFirstName = function (firstName) {
-
-    const firstNameRegExp = new RegExp(
-        '^[A-Za-z]{1,20}$',
-        'g'
-    );
-    let testFirstName = firstNameRegExp.test(firstName.value)
-    let small = inputFirstName.nextElementSibling;
-    if (testFirstName) {
-        small.innerHTML = 'Prénom valide';
-        small.classList.remove('text-danger');
-        small.classList.add('text-success');
-
-    } else {
-        small.innerHTML = "Prénom invalide ! "
-        small.classList.remove('text-success');
-        small.classList.add('text-danger');
-    }
-};
-
-inputFirstName.addEventListener('change', function () {
-    validateFirstName(this);
-});
-
-
-//Validation caratère Ville !!
-const validateCity = function (city) {
-
-    const cityRegExp = new RegExp(
-        '^[a-zA-Z-\\s]{1,50}$',
-        'g'
-    )
-    let testCity = cityRegExp.test(city.value)
-    let small = inputCity.nextElementSibling;
-    if (testCity) {
-        small.innerHTML = 'Ville valide';
-        small.classList.remove('text-danger');
-        small.classList.add('text-success');
-
-    } else {
-        small.innerHTML = "Ville invalide ! "
-        small.classList.remove('text-success');
-        small.classList.add('text-danger');
-    }
-};
-inputCity.addEventListener('change', function () {
-    validateCity(this);
-});
-
-
-//Validation caratère Adresse !!
-const validateAdress = function (adresse) {
-
-    const adresseRegExp = new RegExp(
-        '^[a-zA-Z0-9-_\\s]{1,50}$',
-        'g'
-    )
-    let testAdress = adresseRegExp.test(adresse.value)
-    let small = inputAdress.nextElementSibling;
-    if (testAdress) {
-        small.innerHTML = 'Adresse valide';
-        small.classList.remove('text-danger');
-        small.classList.add('text-success');
-
-    } else {
-        small.innerHTML = "Adresse invalide ! "
-        small.classList.remove('text-success');
-        small.classList.add('text-danger');
-    }
-};
-inputAdress.addEventListener('change', function () {
-    validateAdress(this);
-})
-
 
 // ************************************************************************************************************************************************
+
+//Appel de fonction
+viewCart();
+reccupValueForm();
+validateForm();
+eventClick();
+
+mainCart.removeChild(produitCart);
+produitCart.removeAttribute('id');
--- front/view-produit.js
+++ front/view-produit.js
@@ -7,12 +7,12 @@ let priceProduit = document.getElementById('price-produit');
 const buttonAddPanier = document.getElementById('add-panier');
 
 
-const focusTeddie = async function(){
+const focusTeddie = async function () {
 
     const params = new URLSearchParams(location.search)
-    
+
     let response = await fetch(`http://localhost:3000/api/teddies/${params.get('id')}`);
-    if(response.ok){
+    if (response.ok) {
 
         const data = await response.json();
         //console.log(data);
@@ -21,49 +21,49 @@ const focusTeddie = async function(){
         //console.log(produit);
         descriptionProduit.innerText = produit.description;
         priceProduit.innerText = "prix : " + produit.price + " €";
-        
-        Array.from(nameProduit).forEach(function(element){
+
+        Array.from(nameProduit).forEach(function (element) {
             element.innerText = produit.name
         })
-        
+
         const optionColor = produit.colors;
         //console.log(optionColor);
-        
-        optionColor.forEach(function(element , key){
+
+        optionColor.forEach(function (element, key) {
             colorSelect[key] = new Option(element, key);
         })
-        
+
         console.log(produit._id)
         imgProduit.src = produit.imageUrl;
         buttonAddPanier.addEventListener('click', event => {
-                            
-                let cart = localStorage.getItem('cart');
-                
-                if(cart == null){
-                    cart = [
-                        {
-                            'name': produit.name,
-                            'id': produit._id,
-                            'quantity': 1,
-                            'price': produit.price,
-                            'img': produit.imageUrl,
-                        }
-                    ]
-                } else{
-                    cart = JSON.parse(cart)
-                    let finded = 0;
-                    for(let i = 0; i < cart.length; i++){
-                        if(produit.name === cart[i].name){
-                            cart[i].quantity += 1;
-                            finded = 1;
-                            break
-                        }
+
+            let cart = localStorage.getItem('cart');
+
+            if (cart == null) {
+                cart = [
+                    {
+                        'name': produit.name,
+                        'id': produit._id,
+                        'quantity': 1,
+                        'price': produit.price,
+                        'img': produit.imageUrl,
                     }
-                    if(!finded){
-                        cart.push({'name': produit.name, 'id': produit._id, 'quantity': 1, 'price': produit.price, 'img': produit.imageUrl,})
+                ]
+            } else {
+                cart = JSON.parse(cart)
+                let finded = 0;
+                for (let i = 0; i < cart.length; i++) {
+                    if (produit.name === cart[i].name) {
+                        cart[i].quantity += 1;
+                        finded = 1;
+                        break
                     }
                 }
-                localStorage.setItem('cart', JSON.stringify(cart));
+                if (!finded) {
+                    cart.push({ 'name': produit.name, 'id': produit._id, 'quantity': 1, 'price': produit.price, 'img': produit.imageUrl, })
+                }
+            }
+            localStorage.setItem('cart', JSON.stringify(cart));
         });
     }
 };
