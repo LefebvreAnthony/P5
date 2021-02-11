56d196c1ca842b66e9a1d7e6c9d41a5e31af0bf7
Lefebvre Anthony <anthony.lfb@hotmail.fr>
2021-02-11 10:34:20 +0100

    Vérification formulaire

--- front/panier.js
+++ front/panier.js
@@ -20,7 +20,7 @@ let postForm = {
 };
 
 //création visuel des articles que l'on à ajouté au panier
-function viewCart() {
+const viewCart = function () {
     try {
         if (localStorage.length >= 1) {
             let totalPrice = 0;
@@ -55,40 +55,7 @@ function viewCart() {
     }
 }
 
-//Function Reccup value formulaire 
-const reccupValueForm = function(){
-
-    document.getElementById('inputValidation').addEventListener('click', function (evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        postForm.contact = {
-            firstName: inputFirstName.value,
-            lastName: inputLastName.value,
-            address: inputAdress.value,
-            city: inputCity.value,
-            email: inputMail.value,
-        }
-        fetch('http://localhost:3000/api/teddies/order', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(postForm),
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.json().then(data => {
-                        localStorage.clear();
-                        localStorage.setItem("orderId", data.orderId)
-                        location.href = 'order.html'
-    
-                    })
-                };
-            })
-    });
-};
-
-const eventClick = function(){
+const eventClick = function () {
     //Nos produit button
     document.getElementById('all-products').addEventListener('click', function () {
         window.location.href = '../index.html';
@@ -101,8 +68,9 @@ const eventClick = function(){
         window.alert('Votre panier est supprimé');
     });
 };
+
 // ************************************************************************ SERIE DE VALIDATION FORM ************************************************************************
-const validateInuput = function (input, inputName, regex) {
+const validateInput = function (input, inputName, regex) {
     const regExp = new RegExp(regex, 'g');
     let small = input.nextElementSibling;
 
@@ -112,6 +80,7 @@ const validateInuput = function (input, inputName, regex) {
         small.classList.add('text-success');
         input.classList.remove('border-danger');
         input.classList.add('border-success');
+        return true;
 
     } else {
         small.innerHTML = inputName + " invalide ! ";
@@ -119,44 +88,87 @@ const validateInuput = function (input, inputName, regex) {
         small.classList.add('text-danger');
         input.classList.remove('border-success');
         input.classList.add('border-danger');
+        return false;
 
     }
 };
 
-const validateForm = function(){
+const validateForm = function () {
 
     // Validation caractère Email
     inputMail.addEventListener('change', function () {
-        validateInuput(this, 'Email', '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+        validateInput(this, 'Email', '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
     });
-    
+
     //Validation caratère Nom de famille !!
     inputLastName.addEventListener('change', function () {
-        validateInuput(this, 'Nom', '^[A-Za-z]{1,20}$');
+        validateInput(this, 'Nom', '^[A-Za-z]{1,20}$');
     });
-    
+
     //Validation caratère Prénom !!
     inputFirstName.addEventListener('change', function () {
-        validateInuput(this, 'Prénom', '^[A-Za-z]{1,20}$');
+        validateInput(this, 'Prénom', '^[A-Za-z]{1,20}$');
     });
-    
+
     //Validation caratère Ville !!
     inputCity.addEventListener('change', function () {
-        validateInuput(this, 'Ville', '^[a-zA-Z-\\s]{3,50}$');
+        validateInput(this, 'Ville', '^[a-zA-Z-\\s]{3,50}$');
     });
-    
+
     //Validation caratère Adresse !!
     inputAdress.addEventListener('change', function () {
-        validateInuput(this, 'Adresse', '^[a-zA-Z0-9-_\\s]{3,50}$');
+        validateInput(this, 'Adresse', '^[a-zA-Z0-9-_\\s]{3,50}$');
     });
 };
 
 // ************************************************************************************************************************************************
 
+//Function Reccup value formulaire 
+const reccupValueForm = function () {
+
+    document.getElementById('inputValidation').addEventListener('click', function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        postForm.contact = {
+            firstName: inputFirstName.value,
+            lastName: inputLastName.value,
+            address: inputAdress.value,
+            city: inputCity.value,
+            email: inputMail.value,
+        }
+        if (validateInput(inputMail, 'Email', '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$') &&
+            validateInput(inputLastName, 'Nom', '^[A-Za-z]{1,20}$') &&
+            validateInput(inputFirstName, 'Prénom', '^[A-Za-z]{1,20}$') &&
+            validateInput(inputCity, 'Ville', '^[a-zA-Z-\\s]{3,50}$') &&
+            validateInput(inputAdress, 'Adresse', '^[a-zA-Z0-9-_\\s]{3,50}$')
+        ) {
+
+            fetch('http://localhost:3000/api/teddies/order', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(postForm),
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json().then(data => {
+                            localStorage.clear();
+                            localStorage.setItem("orderId", data.orderId);
+                            location.href = 'order.html';
+                        })
+                    };
+                })
+        } else {
+            alert('Formulaire invalide !')
+        }
+    });
+};
+
 //Appel de fonction
 viewCart();
-reccupValueForm();
 validateForm();
+reccupValueForm();
 eventClick();
 
 mainCart.removeChild(produitCart);
